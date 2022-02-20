5bf974033fecc7e5752a4584ef2975870fc58221
Faith Sodipe <faithsodipe@gmail.com>
2022-02-20 01:25:52 +0100

    used callbacks with named functions in async ops

--- index.ts
+++ index.ts
@@ -1,27 +1,39 @@
-import express from 'express'; //import express library into file
-import logRequest from './middlewares/logger';
-import authCheck  from './middlewares/authenticator';
-import helmet from 'helmet'; //used for api security e.g headers, authentication filters e.t.c
-import langs from './routes/genres';
-import home from './routes/home';
+//ASYNC OPERATIONS
 
-const app = express(); //creates an express application
-app.use(helmet());
+//callbacks: is another method that excecutes after a method has completed.
+//promises
+//async/await
 
-app.use(express.json()) //import json serialization and deserialization in express (express.json()) is a middleware function
-app.use(express.urlencoded({extended:  true})) //import a middleware for us to use form data for POST request instead of raw json body 
-app.use(express.static('assets')); //this loads a middleware that allows usage of static files, e.g images, css and textfiles e.t.c
 
-app.use(authCheck);
-//this is used to create a custom middleware
-app.use(logRequest.logRequest2);
+//1.  USING CALLBACKS -----------------------------------------------
+console.log('Before');
+getUser(3,  getRepos);
 
-//import newly exported courses modules into index module
-app.use('/', home);
-app.use('/api/genres', langs);
+console.log('After'); 
 
-//use environment variable to dynamically get port number
+function displayRepos (repos: []) {
+    console.log(repos);
+ }
 
-const port = process.env.PORT ?? 3000;
+ function getRepos(user: any){
+   getRepositories(user.username, displayRepos);
+ }
 
-app.listen(port , () => console.log(`Listening on port ${port}...`));
+
+
+function  getUser(id:number, callback: Function) {
+  setTimeout(() => {
+   console.log('I am an async function');
+   //after result is ready 
+   callback ({id: id, username: 'classyk'})
+  }, 2000);
+}
+
+function  getRepositories (username: string, callback: Function){
+   setTimeout(() => {
+   console.log('I am another async function');
+
+   callback( ['repo1', 'repo2', 'repo3']);
+      
+   }, 2000);
+}
